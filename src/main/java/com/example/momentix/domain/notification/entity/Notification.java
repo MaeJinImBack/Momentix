8674531421854cb56e4deabded99575aa90c9516
Beforejamni <beforejamn1@gmail.com>
2025-09-06 11:09:30 +0900

    🎉 add : 공지알람 엔티티 초기 세팅

--- src/main/java/com/example/momentix/domain/notification/entity/Notification.java
+++ src/main/java/com/example/momentix/domain/notification/entity/Notification.java
@@ -1,13 +1,28 @@
 package com.example.momentix.domain.notification.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.time.LocalDateTime;
 
 @Entity
 public class Notification {
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long notificationId;
+
+    @Column(name = "messsage")
+    private String message;
+
+    @Column(name = "deliveryTime")
+    @DateTimeFormat
+    private LocalDateTime deliveryTime;
+
+    @Column(name = "reservationStatusType")
+    @Enumerated(EnumType.STRING)
+    private ReservationStatusType reservationStatusType;
+
+
+
 }
