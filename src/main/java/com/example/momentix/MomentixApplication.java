e22f85f5be20948e650c6ecdee87839b5892cbab
hansolChoi29 <ggksthf29@gmail.com>
2025-09-06 09:27:35 +0900

    :sparkles: chore: 비밀번호 임시 암호화 코드 제거 main

--- src/main/java/com/example/momentix/MomentixApplication.java
+++ src/main/java/com/example/momentix/MomentixApplication.java
@@ -11,12 +11,5 @@ public class MomentixApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MomentixApplication.class, args);
-
-        //회원가입 구현 이후에 지울 예정(임시 유저 비밀번호 암호화)
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPassword = "1234";
-        String encoded = encoder.encode(rawPassword);
-        System.out.println("1234 암호화된 비번 = " + encoded);
     }
-
 }
