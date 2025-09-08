9351069a82a8b4d0f1ea7f36a1d6577aa10fe919
hansolChoi29 <ggksthf29@gmail.com>
2025-09-08 15:50:21 +0900

    :white_check_mark: test: 정상 가입 test

--- src/test/java/com/example/momentix/domain/auth/service/SignUpServiceTest.java
+++ src/test/java/com/example/momentix/domain/auth/service/SignUpServiceTest.java
@@ -2,6 +2,7 @@ package com.example.momentix.domain.auth.service;
 
 import com.example.momentix.domain.auth.dto.SignUpRequest;
 import com.example.momentix.domain.auth.repository.SignInRepository;
+import com.example.momentix.domain.users.entity.Users;
 import com.example.momentix.domain.users.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,11 +11,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class SignUpServiceTest {
@@ -42,6 +44,7 @@ public class SignUpServiceTest {
                 "01029388457",
                 null
         );
+        // 아이디 중복 이런 거 없어서 스텁 필요없음
         // when & then
         ResponseStatusException ex = assertThrows(ResponseStatusException.class,
                 () -> signUpService.signUpUser(signUpRequest));
@@ -49,9 +52,55 @@ public class SignUpServiceTest {
         assertEquals(HttpStatus.BAD_REQUEST, ex.getStatusCode());
         assertEquals("400 BAD_REQUEST \"잘못된 요청(비밀번호 불일치)\"", ex.getMessage());
     }
-    //then
 
     // 02. 중복 아이디409
+    @Test
+    public void 중복아이디(){
+
+        // given
+        SignUpRequest signUpRequest = new SignUpRequest(
+                "user1",
+                "1234",
+                "1234",
+                "nickname",
+                "20000219",
+                "01029388457",
+                null
+        );
+        given(signInRepository.existsByUsername("user1")).willReturn(true); //willReturn이란, 어떤 값을 돌려줘라~라고 가짜 객체의 동작을 지정할 때 쓰는 문법
+
+        //when&then
+        ResponseStatusException ex =assertThrows(ResponseStatusException.class,()-> signUpService.signUpUser(signUpRequest));
+        assertEquals(HttpStatus.CONFLICT, ex.getStatusCode());
+        assertEquals("409 CONFLICT \"이미 사용 중인 아이디입니다\"", ex.getMessage());
+
+    }
     
     // 03. 정상가입 userId 반환
+    @Test
+    public void 회원가입정상가입시userId반환(){
+        SignUpRequest signUpRequest = new SignUpRequest(
+                "user1",
+                "1234",
+                "1234",
+                "nickname",
+                "2000-02-19",
+                "01029388457",
+                null
+        );
+        given(signInRepository.existsByUsername("user1")).willReturn(false);
+        given(passwordEncoder.encode("1234")).willReturn("1234");
+        given(userRepository.save(any(Users.class))).willAnswer(i ->{
+            Users u = i.getArgument(0);
+            ReflectionTestUtils.setField(u, "userId", 1L);
+            return u;
+        });
+
+        //when
+        Long userId = signUpService.signUpUser(signUpRequest);
+
+        //then
+        assertNotNull(userId);
+        assertEquals(userId, 1L);
+    }
 }
