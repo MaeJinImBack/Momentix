4d72b13189d099d56b1d091017e71fa742030ebc
hansolChoi29 <ggksthf29@gmail.com>
2025-09-10 13:37:17 +0900

    :white_check_mark: test: 청소년 test

---
+++ src/test/java/com/example/momentix/domain/common/util/AgeUtilTest.java
@@ -0,0 +1,19 @@
+package com.example.momentix.domain.common.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.array;
+
+
+public class AgeUtilTest {
+
+    @Test
+    void 만19세미만_false(){
+        LocalDate birthDate = LocalDate.of(2007,2,19);
+        boolean result=AgeUtil.isOverAge(birthDate,19);
+        assertThat(result).isFalse();
+    }
+
+}
