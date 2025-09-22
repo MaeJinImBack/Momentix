0b10c65f6e78608fac7d723e728f7169aafc6c47
hansolChoi29 <ggksthf29@gmail.com>
2025-09-22 10:34:07 +0900

    :sparkles: feat: 컨트롤러

--- src/main/java/com/example/momentix/domain/paymenthistory/controller/PaymentHistoryController.java
+++ src/main/java/com/example/momentix/domain/paymenthistory/controller/PaymentHistoryController.java
@@ -1,7 +1,17 @@
 package com.example.momentix.domain.paymenthistory.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/payment")
 public class PaymentHistoryController {
+
+    //결제 생성=/reservation/{ticketId}/check-out
+
+    // 결제 수정 =???
+
+    //결제 조회 =/payments/{paymentId}
+
+    //결제 삭제 =/payments/{paymentId}/cancel
 }
