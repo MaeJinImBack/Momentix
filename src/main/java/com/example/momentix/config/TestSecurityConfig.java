2405da6b3e2f96548f583e458d68abce5f06ff61
JIHUN KWAK <rhkrwlgns12@naver.com>
2025-09-24 11:40:26 +0900

    ✨ feat: 성능테스트를 위해 로그인 없이 좌석선점 하는 방식 구현(테스트 완료 후 삭제 예정)

---
+++ src/main/java/com/example/momentix/config/TestSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.momentix.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Profile("load-test")
+@Configuration
+@EnableWebSecurity
+public class TestSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                // ⭐️ [수정] 모든 요청(anyRequest)을 허용(permitAll)하도록 변경합니다.
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
