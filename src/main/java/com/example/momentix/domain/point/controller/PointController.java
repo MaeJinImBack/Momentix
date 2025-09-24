1eb770a7d8b49c061d18a13393c555178eb0642c
hansolChoi29 <ggksthf29@gmail.com>
2025-09-24 12:31:22 +0900

    :sparkles: feat: 컨트롤러 틀 생성

--- src/main/java/com/example/momentix/domain/point/controller/PointController.java
+++ src/main/java/com/example/momentix/domain/point/controller/PointController.java
@@ -2,11 +2,13 @@ package com.example.momentix.domain.point.controller;
 
 
 import com.example.momentix.domain.point.service.PointService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/points")
+@RequestMapping("/users/points")
 public class PointController {
 
     private final PointService pointService;
@@ -16,14 +18,40 @@ public class PointController {
     }
 
     // 내 포인트 조회
+    @GetMapping("/me")
+    public ResponseEntity<> me(
 
+    ){
+
+    }
 
     // 즉시 적립
+    @GetMapping("/earn")
+    public ResponseEntity<> earn(
 
+    ){
+
+    }
     // 사용(차감)
+    @GetMapping("/use")
+    public ResponseEntity<> use(
 
+    ){
+
+    }
 
     // 적립 에정
-    
+    @GetMapping("/pending/earn")
+    public ResponseEntity<> earnPending(
+
+    ){
+
+    }
     //예정 해제
+    @GetMapping("/pending/release")
+    public ResponseEntity<> releasePending(
+
+    ){
+
+    }
 }
